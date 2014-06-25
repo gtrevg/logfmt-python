dcdce45e9eb2ff77fedde7b4914ff8b3e5060a8d
Trevor Gattis <ggatti000@trevor-worktop.local>
2014-06-25 09:48:16 -0700

    put in a check for python version as the conversion from float to string is different

--- tests/test_formatter.py
+++ tests/test_formatter.py
@@ -2,6 +2,8 @@
 from logfmt.formatter import format_line
 from unittest import TestCase
 from collections import OrderedDict
+import sys
+
 
 class FormatterTestCase(TestCase):
     def test_empty_log_line(self):
@@ -20,10 +22,15 @@ class FormatterTestCase(TestCase):
         data = format_line( OrderedDict([("key1", -1), ("key2", 2342342)]) )
         self.assertEqual(data, "key1=-1 key2=2342342")
 
-    # By default, truncated to 12 digits total
+    # In python 2.7, truncated to 12 digits total.  3.x does 16 
     def test_float_value(self):
         data = format_line( OrderedDict([("key1", 342.23424), ("key2", -234234234.2342342)]) )
-        self.assertEqual(data, "key1=342.23424 key2=-234234234.234")
+        
+	if sys.version_info < (3, 0):
+            self.assertEqual(data, "key1=342.23424 key2=-234234234.234")
+        else:
+            234234234.2342342
+            self.assertEqual(data, "key1=342.23424 key2=-234234234.2342342")
 
     # Essentially, pre format your floats to strings
     def test_custom_float_format(self):
